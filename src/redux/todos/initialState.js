9687f2ce7049e0445d79740903913c452750a5cd
Abdur Rahman <abdurrahman.offical.63@gmail.com>
2022-10-06 03:04:48 +0600

    added redux

---
+++ src/redux/todos/initialState.js
@@ -0,0 +1,15 @@
+const initialState = [
+  {
+    id: 1,
+    text: "learn React js",
+    completed: true,
+  },
+  {
+    id: 2,
+    text: "learn Redux",
+    completed: false,
+    color: "red",
+  },
+];
+
+export default initialState;
